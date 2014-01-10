5fe26de9e4bd45b0a6b41608f7f118664e40ec2f
Adam Krajewski <admkrajewski@gmail.com>
2014-01-11 00:51:04 +0100

    - Routing mechanisms introduced

--- TOPT.NetworkSimulator.Program/Program.cs
+++ TOPT.NetworkSimulator.Program/Program.cs
@@ -4,13 +4,42 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System.IO;
+using TOPT.NetworkSimulator.Routing;
+
 namespace TOPT.NetworkSimulator.Program
 {
-  class Program
-  {
-    static void Main( string[ ] args )
+    class Program
     {
-        //ścierwo
+        static void Main(string[] args)
+        {
+            RoutingTests(args);
+        }
+
+        private static void RoutingTests(string[] args)
+        {
+            var filename = "tinyEWD.txt";
+            var lines = File.ReadAllLines(filename);
+            var vertexCount = int.Parse(lines.First());
+            var graph = new Digraph(vertexCount);
+            foreach (var line in lines.Skip(1))
+            {
+                var splitted = line.Split(' ');
+                var from = int.Parse(splitted[0]);
+                var to = int.Parse(splitted[1]);
+                graph.addEdge(new DirectedEdge(from, to));
+            }
+            var dijkstra = new DijkstraSP(graph, 0);
+            for (var i = 1; i < vertexCount; i++)
+            {
+                Console.Write("0 to {0}: ", i);
+                foreach (var edge in dijkstra.pathTo(i))
+                {
+                    Console.Write("{0}->{1}", edge.From, edge.To);
+                }
+                Console.Write("\n");
+            }
+        }
+
     }
-  }
 }
---
+++ TOPT.NetworkSimulator.Routing/Digraph.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TOPT.NetworkSimulator.Routing
+{
+    public class Digraph
+    {
+        private List<DirectedEdge>[] adj;
+
+        public int VertexCount 
+        { 
+            get { return adj.Length; }
+            
+        }
+
+        public Digraph(int v)
+        {
+            adj = new List<DirectedEdge>[v];
+            for(var i = 0; i < v; i++)
+            {
+                adj[i] = new List<DirectedEdge>();
+            }
+        }
+
+        public void addEdge(int from, int to)
+        {
+            adj[from].Add(new DirectedEdge(from, to));
+        }
+
+        public void addEdge(DirectedEdge de)
+        {
+            adj[de.From].Add(de);
+        }
+
+        public List<DirectedEdge> edges(int v)
+        {
+            return adj[v];
+        }
+    }
+}
